4a540ae74e6aeb8789990e733cb0cf918275e10f
Adam Gonzales <adamgonzales24@icloud.com>
2021-11-17 03:02:43 -0600

    Finished memory game

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Grab things
 const section = document.querySelector("section");
 const playerLivesCount = document.querySelector("span");
-const playerLives = 6;
+let playerLives = 8;
 
 //link text
 playerLivesCount.textContent = playerLives;
@@ -61,8 +61,66 @@ const cardGenerator = () => {
 
 //Check Cards
 const checkCards = (e) => {
+  console.log(e);
   const clickedCard = e.target;
   clickedCard.classList.add("flipped");
+  const flippedCards = document.querySelectorAll(".flipped");
+  const toggleCard = document.querySelectorAll(".toggleCard");
+  console.log(flippedCards);
+
+  //logic
+  if (flippedCards.length === 2) {
+    if (
+      flippedCards[0].getAttribute("name") ===
+      flippedCards[1].getAttribute("name")
+    ) {
+      console.log("match");
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+
+        card.style.pointerEvents = "none";
+      });
+    } else {
+      console.log("wrong");
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+        setTimeout(() => card.classList.remove("toggleCard"), 1000);
+      });
+      playerLives--;
+      console.log(playerLives);
+      playerLivesCount.textContent = playerLives;
+      if (playerLives === 0) {
+        restart("Try Again");
+      }
+    }
+  }
+  //Run a check to see if we won the game
+  if (toggleCard.length === 16) {
+    setTimeout(() => {
+      restart("You Won!");
+    }, 1000);
+  }
+};
+
+//Restart
+const restart = (text) => {
+  let cardData = randomize();
+  let faces = document.querySelectorAll(".face");
+  let cards = document.querySelectorAll(".card");
+  section.style.pointerEvents = "none";
+  cardData.forEach((item, index) => {
+    cards[index].classList.remove("toggleCard");
+    //Randomize
+    setTimeout(() => {
+      cards[index].style.pointerEvents = "all";
+      faces[index].src = item.imgScr;
+      cards[index].setAttribute("name", item.name);
+      section.style.pointerEvents = "all";
+    }, 1000);
+  });
+  playerLives = 8;
+  playerLivesCount.textContent = playerLives;
+  setTimeout(() => window.alert(text), 100);
 };
 
 cardGenerator();
